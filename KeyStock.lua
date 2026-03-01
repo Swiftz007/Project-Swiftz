384f901394be82b9e381cba97b5287c41087ec72
Swiftz007 <cxllme14zx@gmail.com>
2026-03-02 00:23:38 +0700

    Add comments to keys in correctKeys table
    Updated correctKeys table to include comments for each key.

--- KeyStock.lua
+++ KeyStock.lua
@@ -2,17 +2,17 @@ local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 
 local correctKeys = {
-    "X2SX-1234-5678-RRRR-TTTT"
-    "S9KX-4TQ7-2MZL-8PWR-6HJD",
-    "SB3F-7XQ2-L9VN-5KTR-1ZPM",
-    "S7DL-2QWX-8VJN-4RKP-9TGH",
-    "SMX2-6QPL-3VKR-9ZWT-5HNF",
-    "S4ZT-8KQM-1XVR-6LPD-2JHG",
-    "SQ9L-5VXT-7KRP-3ZMN-8HWF",
-    "S2XK-9LQV-4TRP-7ZHD-6MJN",
-    "SR7P-3KXT-9VQL-2HZN-5MFW",
-    "S8QV-1ZKP-6XTR-4LHM-9JWD",
-    "ST5X-9QVL-2KRP-8ZMN-3HJF"
+    "X2SX-1234-5678-RRRR-TTTT", -- Admin key
+    "S9KX-4TQ7-2MZL-8PWR-6HJD", -- User key 1
+    "SB3F-7XQ2-L9VN-5KTR-1ZPM", -- User key 2
+    "S7DL-2QWX-8VJN-4RKP-9TGH", -- User key 3
+    "SMX2-6QPL-3VKR-9ZWT-5HNF", -- User key 4
+    "S4ZT-8KQM-1XVR-6LPD-2JHG", -- User key 5
+    "SQ9L-5VXT-7KRP-3ZMN-8HWF", -- User key 6
+    "S2XK-9LQV-4TRP-7ZHD-6MJN", -- User key 7
+    "SR7P-3KXT-9VQL-2HZN-5MFW", -- User key 8
+    "S8QV-1ZKP-6XTR-4LHM-9JWD", -- User key 9
+    "ST5X-9QVL-2KRP-8ZMN-3HJF" -- User key 10
 }
 
 local key = getgenv().Key
