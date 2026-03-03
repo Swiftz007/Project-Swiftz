b6c9d9df8cb08e7e47f787900d37ca68ea92e83e
Swiftz007 <cxllme14zx@gmail.com>
2026-03-04 02:32:00 +0700

    Add user keys 51 to 100 in KeyStock.lua

--- KeyStock.lua
+++ KeyStock.lua
@@ -52,7 +52,57 @@ local correctKeys = {
     "S1KP-9ZXL-5QRT-8VMN-3HJD", -- User key 47
     "S6ZX-2KLP-7QRT-4VMN-9HWF", -- User key 48
     "S9KP-5ZXL-3QRT-1VMN-8HJD", -- User key 49
-    "S7ZX-4KLP-2QRT-6VMN-5HFD"  -- User key 50
+    "S7ZX-4KLP-2QRT-6VMN-5HFD", -- User key 50
+    "A4KD-7PQL-9XTR-2VMN-6HJF", -- User key 51
+    "Z9LM-3QXT-5KRP-8VHN-1DWF", -- User key 52
+    "R2PX-8LQT-6VMK-1ZHN-4JFD", -- User key 53
+    "Y7TK-1QPL-8VXR-5MND-9HWF", -- User key 54
+    "H3QV-9LXP-2KTR-7MND-6JWF", -- User key 55
+    "K8LP-4QXT-7VNM-2RHD-5JWF", -- User key 56
+    "V1QT-6KPL-9XRM-3HND-8JWF", -- User key 57
+    "D5KP-2LQT-8VMR-1ZHN-7JWF", -- User key 58
+    "J9LX-7QTP-4KRM-6VND-2HWF", -- User key 59
+    "P3QT-8LKM-1XRV-9HND-5JWF", -- User key 60
+    "F6KP-1LQT-7VMR-3ZHN-9JWD", -- User key 61
+    "T2LX-9QKP-5VRM-8HND-4JWF", -- User key 62
+    "W8QT-3LKM-6XRV-1HND-7JWF", -- User key 63
+    "B4KP-7LQT-2VMR-9ZHN-5JWF", -- User key 64
+    "N1LX-5QTP-8KRM-4VND-6HWF", -- User key 65
+    "Q7RT-2LKM-9XVP-6HND-3JWF", -- User key 66
+    "C9KP-4LQT-1VMR-7ZHN-8JWF", -- User key 67
+    "U3LX-8QTP-6KRM-2VND-5HWF", -- User key 68
+    "E5QT-1LKM-7XRV-9HND-4JWF", -- User key 69
+    "M8KP-6LQT-3VMR-2ZHN-1JWF", -- User key 70
+    "G2LX-7QTP-9KRM-5VND-8HWF", -- User key 71
+    "L4QT-9KPM-2XRV-7HND-6JWF", -- User key 72
+    "O7KP-3LQT-8VMR-4ZHN-5JWF", -- User key 73
+    "I1LX-6QTP-4KRM-9VND-2HWF", -- User key 74
+    "X8QT-5LKM-3VRP-1HND-7JWF", -- User key 75
+    "Z2KP-9LQT-6VMR-8HND-4JWF", -- User key 76
+    "R5LX-1QTP-7KRM-3VND-9HWF", -- User key 77
+    "Y9QT-4LKM-8XRP-2HND-6JWF", -- User key 78
+    "H6KP-2LQT-5VMR-1ZHN-7JWF", -- User key 79
+    "K3LX-8QTP-9KRM-4VND-5HWF", -- User key 80
+    "V7QT-1LKM-6XRP-9HND-2JWF", -- User key 81
+    "D9KP-5LQT-3VMR-7ZHN-8JWF", -- User key 82
+    "J4LX-2QTP-8KRM-6VND-1HWF", -- User key 83
+    "P8QT-7LKM-1XRP-5HND-9JWF", -- User key 84
+    "F3KP-6LQT-9VMR-2ZHN-4JWF", -- User key 85
+    "T7LX-9QTP-4KRM-8VND-3HWF", -- User key 86
+    "W2QT-8LKM-5XRP-1HND-6JWF", -- User key 87
+    "B6KP-1LQT-7VMR-9ZHN-2JWF", -- User key 88
+    "N9LX-4QTP-2KRM-7VND-5HWF", -- User key 89
+    "Q3QT-6LKM-8XRP-4HND-1JWF", -- User key 90
+    "C7KP-2LQT-5VMR-3ZHN-9JWF", -- User key 91
+    "U1LX-9QTP-6KRM-8VND-4HWF", -- User key 92
+    "E8QT-3LKM-7XRP-2HND-5JWF", -- User key 93
+    "M2KP-4LQT-9VMR-6ZHN-7JWF", -- User key 94
+    "G5LX-7QTP-1KRM-3VND-8HWF", -- User key 95
+    "L9QT-2KPM-6XRV-5HND-4JWF", -- User key 96
+    "O4KP-8LQT-3VMR-1ZHN-9JWF", -- User key 97
+    "I7LX-5QTP-9KRM-2VND-6HWF", -- User key 98
+    "X1QT-6LKM-4VRP-8HND-3JWF", -- User key 99
+    "Z8KP-3LQT-7VMR-5HND-2JWF"  -- User key 100
 }
 
 local key = getgenv().Key
