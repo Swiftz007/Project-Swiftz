0581795a519f195ddd0b30cdaff0181005e89719
Swiftz007 <cxllme14zx@gmail.com>
2026-05-04 06:59:26 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
--- #Fix add reaper theme
+-- #Fix add reaper theme 2
 local a, b = {
     {
         1,
@@ -15,8 +15,7 @@ local a, b = {
 			        {50, "ModuleScript", {"Dark"}},
                     {52, "ModuleScript", {"Light"}},
                     {51, "ModuleScript", {"Darker"}},
-                    {53, "ModuleScript", {"Rose"}},
-					{54, "ModuleScript", {"Reaper"}},
+                    {53, "ModuleScript", {"Reaper"}},
                     {49, "ModuleScript", {"Aqua"}},
                     {48, "ModuleScript", {"Amethyst"}}
                 }
@@ -5255,47 +5254,7 @@ local aa = {
     [53] = function()
         local aa, ab, ac, ad, ae = b(53)
         return {
-            Name = "Rose",
-            Accent = Color3.fromRGB(180, 55, 90),
-            AcrylicMain = Color3.fromRGB(40, 40, 40),
-            AcrylicBorder = Color3.fromRGB(130, 90, 110),
-            AcrylicGradient = ColorSequence.new(Color3.fromRGB(190, 60, 135), Color3.fromRGB(165, 50, 70)),
-            AcrylicNoise = 0.92,
-            TitleBarLine = Color3.fromRGB(140, 85, 105),
-            Tab = Color3.fromRGB(180, 140, 160),
-            Element = Color3.fromRGB(200, 120, 170),
-            ElementBorder = Color3.fromRGB(110, 70, 85),
-            InElementBorder = Color3.fromRGB(120, 90, 90),
-            ElementTransparency = 0.86,
-            ToggleSlider = Color3.fromRGB(200, 120, 170),
-            ToggleToggled = Color3.fromRGB(0, 0, 0),
-            SliderRail = Color3.fromRGB(200, 120, 170),
-            DropdownFrame = Color3.fromRGB(200, 160, 180),
-            DropdownHolder = Color3.fromRGB(120, 50, 75),
-            DropdownBorder = Color3.fromRGB(90, 40, 55),
-            DropdownOption = Color3.fromRGB(200, 120, 170),
-            Keybind = Color3.fromRGB(200, 120, 170),
-            Input = Color3.fromRGB(200, 120, 170),
-            InputFocused = Color3.fromRGB(20, 10, 30),
-            InputIndicator = Color3.fromRGB(170, 150, 190),
-            Dialog = Color3.fromRGB(120, 50, 75),
-            DialogHolder = Color3.fromRGB(95, 40, 60),
-            DialogHolderLine = Color3.fromRGB(90, 35, 55),
-            DialogButton = Color3.fromRGB(120, 50, 75),
-            DialogButtonBorder = Color3.fromRGB(155, 90, 115),
-            DialogBorder = Color3.fromRGB(100, 70, 90),
-            DialogInput = Color3.fromRGB(135, 55, 80),
-            DialogInputLine = Color3.fromRGB(190, 160, 180),
-            Text = Color3.fromRGB(240, 240, 240),
-            SubText = Color3.fromRGB(170, 170, 170),
-            Hover = Color3.fromRGB(200, 120, 170),
-            HoverChange = 0.04
-        }
-    end,
-    [54] = function()
-        local aa, ab, ac, ad, ae = b(54)
-        return {
-     	    Name = "Reaper",
+            Name = "Reaper",
             Accent = Color3.fromRGB(180, 40, 40),
 	        AcrylicMain = Color3.fromRGB(30, 30, 30),
 		    AcrylicBorder = Color3.fromRGB(60, 60, 60),
