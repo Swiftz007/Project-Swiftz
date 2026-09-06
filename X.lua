0d323d39cfe84743a2544464c1059566b2c7287f
REAPER PROJECT <cxllme14zx@gmail.com>
2026-09-07 02:17:35 +0700

    Update X.lua

--- X.lua
+++ X.lua
@@ -23,25 +23,25 @@ if game.PlaceId == VD then
     mainScript = "https://raw.githubusercontent.com/x2sxqz/Violence-District/refs/heads/main/eng.lua"
 
 elseif game.PlaceId == A then
-    mainScript = ""
+    mainScript = "https://reaperxsecurity.vercel.app/A"
 
 elseif game.PlaceId == B then
-    mainScript = ""
+    mainScript = "https://reaperxsecurity.vercel.app/B"
 
 elseif game.PlaceId == C then
-    mainScript = ""
+    mainScript = "https://reaperxsecurity.vercel.app/C"
 
 elseif game.PlaceId == D then
-    mainScript = ""
+    mainScript = "https://reaperxsecurity.vercel.app/D"
 
 elseif game.PlaceId == E then
-    mainScript = ""
+    mainScript = "https://reaperxsecurity.vercel.app/E"
 
 elseif game.PlaceId == F then
-    mainScript = ""
+    mainScript = "https://reaperxsecurity.vercel.app/F"
 
 elseif game.PlaceId == G then
-    mainScript = ""
+    mainScript = "https://reaperxsecurity.vercel.app/G"
 
 elseif _G.Script_Language == "Thai" then
     mainScript = "https://raw.githubusercontent.com/x2sxqz/Normal/refs/heads/main/Thaixyz.lua"
