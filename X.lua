e23a91e9dd5b65a51bc91e3e99ee85dea3fac105
REAPER PROJECT <cxllme14zx@gmail.com>
2026-09-07 02:11:00 +0700

    Update X.lua

--- X.lua
+++ X.lua
@@ -1,5 +1,11 @@
 local VD = 93978595733734
-
+local A = nil
+local B = nil
+local C = nil
+local D = nil
+local E = nil
+local F = nil
+local G = nil
 
 local function runScript(url)
     local success, result = pcall(function()
@@ -15,8 +21,31 @@ end
 local mainScript
 if game.PlaceId == VD then
     mainScript = "https://raw.githubusercontent.com/x2sxqz/Violence-District/refs/heads/main/eng.lua"
+
+elseif game.PlaceId == A then
+    mainScript = ""
+
+elseif game.PlaceId == B then
+    mainScript = ""
+
+elseif game.PlaceId == C then
+    mainScript = ""
+
+elseif game.PlaceId == D then
+    mainScript = ""
+
+elseif game.PlaceId == E then
+    mainScript = ""
+
+elseif game.PlaceId == F then
+    mainScript = ""
+
+elseif game.PlaceId == G then
+    mainScript = ""
+
 elseif _G.Script_Language == "Thai" then
     mainScript = "https://raw.githubusercontent.com/x2sxqz/Normal/refs/heads/main/Thaixyz.lua"
+
 else
     mainScript = "https://raw.githubusercontent.com/x2sxqz/Normal/refs/heads/main/kingxyz.lua"
 end
